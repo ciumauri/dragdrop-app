e76d0da1d9c7d5c7cd5a591c4f33c986ceac0f49
Mauricio <mngoliveira@gmail.com>
2021-08-17 00:07:47 -0300

    Inclusão dos arquivos no Git

---
+++ script.js
@@ -0,0 +1,46 @@
+// Initial Data
+
+
+// Events
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('dragstart', dragStart);
+    item.addEventListener('dragend', dragEnd);
+});
+
+document.querySelectorAll('.area').forEach(area => {
+    area.addEventListener('dragover', dragOver);
+    area.addEventListener('dragleave', dragLeave);
+    area.addEventListener('drop', drop);
+})
+
+// Functions Item
+
+function dragStart(e) {
+    e.currentTarget.classList.add('dragging');
+}
+
+function dragEnd(e) {
+    e.currentTarget.classList.remove('dragging');
+}
+
+//Functions Area
+function dragOver(e) {
+    if (e.currentTarget.querySelector('.item') === null) {
+        e.preventDefault();
+        e.currentTarget.classList.add('hover');
+    }
+}
+
+function dragLeave(e) {
+    e.currentTarget.classList.remove('hover');
+}
+
+function drop(e) {
+    e.currentTarget.classList.remove('hover');
+
+    let dragItem = document.querySelector('.item.dragging');
+
+    if (e.currentTarget.querySelector('.item') === null) {
+        e.currentTarget.appendChild(dragItem);
+    }
+}
